9930f6408a61c77cc538f83d49f4d53ce781fe91
giterdun345 <46477620+giterdun345@users.noreply.github.com>
2020-09-27 23:48:37 -0500

    demo data added

--- client/src/components/dashboard/reporting/Layout/Hud/Chart1.js
+++ client/src/components/dashboard/reporting/Layout/Hud/Chart1.js
@@ -18,35 +18,35 @@ const Chart1 = (props) => {
 
    let option = {
         dataset: {
-            // source: [
-            //     ['Amount', 'Service'],
-            //     [89, 'Check-in'],
-            //     [57, 'Classroom Presentation'],
-            //     [74, 'Session'],
-            //     [50, 'Group Session'],
-            //     [89, 'Crisis Intervention'],
-            //     [68, 'Home Visit'],
-            //     [19, 'Meeting'],
-            //     [10, 'Parent Contact'],
-            //     [32, 'CP Referral'],
-            //     [32, 'Referral'],
-            //     [32, 'Discharge']
-            // ]
-            source:[
+            source: [
                 ['Amount', 'Service'],
-                // [tallyServices(props.checks, "checks", moment().month()), 'Check-in'],
-                // [tallyServices(props.presentations, "presentations", moment().month()), 'Classroom Presentation'],
-                // [tallyServices(props.sessions, "sessions", moment().month()), 'Session'],
-                // [tallyServices(props.groups, "groups", moment().month()), 'Group Session'],
-                // [tallyServices(props.interventions, "interventions", moment().month()), 'Crisis Intervention'],
-                // [tallyServices(props.agencies, "agencies", moment().month()), 'Other Methods'],
-                // [tallyServices(props.visits, "visits", moment().month()), 'Home Visit'],
-                // [tallyServices(props.meeting, "meets", moment().month()), 'Meeting'],
-                [tallyServices(props.parent, "parents", moment().month()), 'Parent Contact'],
-                [tallyServices(props.cpref, "cps", moment().month()), 'CP Referral'],
-                [tallyServices(props.referrals, "referrals", moment().month()), 'Referral'],
-                [tallyServices(props.discharges, "discharges", moment().month()), 'Discharge']
+                [89, 'Check-in'],
+                [57, 'Classroom Presentation'],
+                [74, 'Session'],
+                [50, 'Group Session'],
+                [89, 'Crisis Intervention'],
+                [68, 'Home Visit'],
+                [19, 'Meeting'],
+                [10, 'Parent Contact'],
+                [32, 'CP Referral'],
+                [32, 'Referral'],
+                [32, 'Discharge']
             ]
+            // source:[
+            //     ['Amount', 'Service'],
+            //     // [tallyServices(props.checks, "checks", moment().month()), 'Check-in'],
+            //     // [tallyServices(props.presentations, "presentations", moment().month()), 'Classroom Presentation'],
+            //     // [tallyServices(props.sessions, "sessions", moment().month()), 'Session'],
+            //     // [tallyServices(props.groups, "groups", moment().month()), 'Group Session'],
+            //     // [tallyServices(props.interventions, "interventions", moment().month()), 'Crisis Intervention'],
+            //     // [tallyServices(props.agencies, "agencies", moment().month()), 'Other Methods'],
+            //     // [tallyServices(props.visits, "visits", moment().month()), 'Home Visit'],
+            //     // [tallyServices(props.meeting, "meets", moment().month()), 'Meeting'],
+            //     [tallyServices(props.parent, "parents", moment().month()), 'Parent Contact'],
+            //     [tallyServices(props.cpref, "cps", moment().month()), 'CP Referral'],
+            //     [tallyServices(props.referrals, "referrals", moment().month()), 'Referral'],
+            //     [tallyServices(props.discharges, "discharges", moment().month()), 'Discharge']
+            // ]
         },
         grid: {containLabel: true},
         xAxis: {name: 'amt'},
