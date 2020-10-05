cdb2fc5d59095a1120e352fb8e3f5bb961efd7cc
giterdun345 <46477620+giterdun345@users.noreply.github.com>
2020-10-05 16:11:02 -0500

    fnd

--- client/src/components/dashboard/reporting/Layout/Hud/BigChart.js
+++ client/src/components/dashboard/reporting/Layout/Hud/BigChart.js
@@ -4,9 +4,9 @@ import ReactEcharts from 'echarts-for-react';
 import moment from 'moment'
 
 const BigChart = (props) =>{
-    console.log(filterQuery("Sir John A. Cumber", props.visits, 9, "visits"))
-    console.log(filterQuery("Sir John A. Cumber", props.meets, 9, "meets"))
-    console.log(filterQuery("Sir John A. Cumber", props.sessions, 9, "sessions"))
+    console.log(filterQuery("Sir John A. Cumber", props.visits, 10, "visits"))
+    console.log(filterQuery("Sir John A. Cumber", props.meets, 10, "meets"))
+    console.log(filterQuery("Sir John A. Cumber", props.sessions, 10, "sessions"))
 
     function sumMonth(query){
     let total = 0
@@ -28,8 +28,6 @@ function filterQuery(schoolKey, arr, month, column){
     // month must be (month - 1) due to indexing with moment/
     let result = []
     for(let index in arr){
-        console.log(arr[index].school)
-        console.log(moment(arr[index].mon).month() === month)
         if((arr[index].school === schoolKey) && (moment(arr[index].mon).month() === month)){
            result.push(arr[index][column])
         }
