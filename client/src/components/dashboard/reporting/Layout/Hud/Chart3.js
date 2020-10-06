7041a1d4344bfef8c682058ac19888d4497acac7
giterdun345 <46477620+giterdun345@users.noreply.github.com>
2020-10-06 01:47:38 -0500

    ent total

--- client/src/components/dashboard/reporting/Layout/Hud/Chart3.js
+++ client/src/components/dashboard/reporting/Layout/Hud/Chart3.js
@@ -9,9 +9,9 @@ const Chart3 = (props) => {
         let total = 0
         for(var i =0; i < props.engaged.length; i++){
             if((props.engaged[i].school === schoolName)){
-                console.log("thisMonth", thisMonth, "taken", props.engaged[i].mon, "moment", moment(props.engaged[i].mon).month())
+                // console.log("thisMonth", thisMonth, "taken", props.engaged[i].mon, "moment", moment(props.engaged[i].mon).month())
                 if(moment(props.engaged[i].mon).month() === month){
-                    console.log("entered")
+                    console.log(parseInt(props.engaged[i].students, 10))
                     total += parseInt(props.engaged[i].students, 10)
                     console.log(total)
                 }
