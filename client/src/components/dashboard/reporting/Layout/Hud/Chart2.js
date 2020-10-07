a66c667871ad5d689ec61935ad0da9ba1d453c93
giterdun345 <46477620+giterdun345@users.noreply.github.com>
2020-10-07 15:56:10 -0500

    heroku failed

--- client/src/components/dashboard/reporting/Layout/Hud/Chart2.js
+++ client/src/components/dashboard/reporting/Layout/Hud/Chart2.js
@@ -7,9 +7,11 @@ return(
     <div className='justify-content-center'>
         <p className='mt-5 font-weight-bold font-italic text-muted'> 
             You have questions and I have answers; let me know what answers you would like to see on the dashboard. You can email me through the contact tab &nbsp; 
+            
             <span>
                 <a href='http://kettereronline.tech/'>here</a>
             </span>    
+        
         </p>
     </div>
 )
